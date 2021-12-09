a835af3041169090d84e028c8089114afdfa67fe
Mohammad Ahmadi <webdev.ahmadi@gmail.com>
2021-12-10 00:28:10 +0330

    Add all project files

---
+++ BlazorForRedis/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorForRedis.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                SlidingExpiration = unusedExpireTime,
+                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60),
+            };
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if (jsonData is null)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
